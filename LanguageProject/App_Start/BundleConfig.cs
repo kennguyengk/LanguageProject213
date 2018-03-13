045cab51d26d2c3ba69f4b309edbc6462b2cba91
admin <admin@KEN_NGUYEN>
2018-03-12 23:57:35 -0700

    Add project files.

---
+++ LanguageProject/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace LanguageProject
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+
+        public static void RegisterBundles(BundleCollection bundles){
+
+            bundles.Add(new ScriptBundle("~/bundles/core-library").Include(
+                     "~/Scripts/library/jquery-1.11.0.min.js",
+                     "~/Scripts/library/bootstrap.min.js",
+                     "~/Scripts/library/jquery.owl.carousel.js",
+                     "~/Scripts/library/jquery.appear.min.js",
+                     "~/Scripts/library/perfect-scrollbar.min.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/main-script").Include(
+                    "~/Scripts/scripts.js"    
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/library/bootstrap.min.css",
+                    "~/Content/library/font-awesome.min.css",
+                    "~/Content/library/owl.carousel.css.css",
+                    "~/Content/library/md-font.css",
+                    "~/Content/style.css"
+
+            ));
+        }
+    }
+}
---
+++ LanguageProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Login
+
+        public ActionResult Login() {
+
+            return View();
+        }
+
+        // Register
+        public ActionResult Registers() {
+
+            return View();
+
+        }
+    }
+}
