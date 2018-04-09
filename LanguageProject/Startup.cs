c45be52248cfeee4bba2e9e14d2902c47ed602a0
Parth Jaswal <jaswalparth@gmail.com>
2018-04-09 01:38:18 -0400

    Restored

--- LanguageProject/Startup.cs
+++ LanguageProject/Startup.cs
@@ -13,8 +13,7 @@ namespace LanguageProject
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             ConfigureAuth(app);
-            Console.WriteLine();
-            Console.WriteLine();
+            
         }
 
         
